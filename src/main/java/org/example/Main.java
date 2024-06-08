d69d33a9cc12b9eeddd946fceb287b58c601342b
msiewierska <marta.siewierska-stanczak@creativestyle.pl>
2024-06-08 12:57:45 +0200

    Add console command to generate report

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String path;
+        String type;
+        String output;
+
+        try {
+            path = args[0];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Missing file path. Add path");
+            return;
+        }
+
+        try {
+            type = args[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Missing report type. Add type");
+            return;
+        }
+
+        try {
+            output = args[2];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            output = "console";
+        }
+
+        System.out.println("Generate report for path: " + path);
+        System.out.println("Report type: " + type);
+        System.out.println("Output: " + output);
     }
 }
