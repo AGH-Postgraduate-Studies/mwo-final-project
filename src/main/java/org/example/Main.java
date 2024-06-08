f0f32fe944f646ce72c7745abc6a1513b9f53bc8
msiewierska <marta.siewierska-stanczak@creativestyle.pl>
2024-06-08 15:15:53 +0200

    Gather data from folder not file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,9 +8,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "data/simple/2012/01/Nowak_Piotr.xls";
-        List<WorkerTimeSheet> list = ExcelReader.readExcel(path);
+        String path = "data/simple/2012";
 
-        ReportOneGenerator.generateReportOne((List<WorkerTimeSheet>) list, path);
+        ReportOneGenerator.generateReportOne(path);
     }
 }
--- src/main/java/org/example/utils/ReportOneGenerator.java
+++ src/main/java/org/example/utils/ReportOneGenerator.java
@@ -2,10 +2,28 @@ package org.example.utils;
 
 import org.example.model.WorkerTimeSheet;
 
+import java.io.File;
 import java.util.List;
 
 public class ReportOneGenerator {
-    public static void generateReportOne(List<WorkerTimeSheet> list, String path) {
+
+    public static void generateReportOne(String folderPath) {
+        File folder = new File(folderPath);
+        if (folder.isDirectory()) {
+            File[] listOfFolders = folder.listFiles();
+            assert listOfFolders != null;
+            for (File childFolder : listOfFolders)
+            {
+                generateReportOne(folderPath + "/" + childFolder.getName());
+            }
+        } else {
+            ExcelReader.readExcel(folderPath);
+            List<WorkerTimeSheet> list = ExcelReader.readExcel(folderPath);
+            ReportOneGenerator.getHoursFromFile((List<WorkerTimeSheet>) list, folderPath);
+        }
+    }
+
+    public static void getHoursFromFile(List<WorkerTimeSheet> list, String path) {
         int hours = 0;
 
         for (WorkerTimeSheet workerTimeSheet : list) {
@@ -16,9 +34,11 @@ public class ReportOneGenerator {
             System.out.println("Chosen list is empty.");
             System.out.println("Report can't be generated.");
         } else {
-            System.out.println("Report one generated successfully.");
-            System.out.println("Path to file: " +path);
-            System.out.println("Total count hours in generated report: " + hours);
+//            System.out.println("Report one generated successfully.");
+//
+//            System.out.println("Path to file: " +path);
+
+            System.out.println("Total count hours in " + "path to file: " + path + " is: " + hours);
         }
     }
 }
