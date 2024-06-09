4ff76714b2afb659447a74799d47eb7627409f8a
Student <student@d17-428-pc-02.ipa.ii.agh.edu.pl>
2024-06-09 10:38:27 +0200

    Create constructor in ReportByPersonAdd

--- src/main/java/org/example/model/ReportByPersonAdd.java
+++ src/main/java/org/example/model/ReportByPersonAdd.java
@@ -3,9 +3,13 @@ package org.example.model;
 import java.util.List;
 
 public class ReportByPersonAdd extends Report {
+    public ReportByPersonAdd(String path) {
+        super(path);
+    }
+
     @Override
     protected List<List<Object>> getData(String path) {
-
+        return null;
     }
 
     @Override
