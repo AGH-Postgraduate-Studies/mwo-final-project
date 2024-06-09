a1663c4f94b9bcc48f870d18be14f6953218431f
Maciej Szostak <maciek.szostak55@gmail.com>
2024-06-09 14:19:50 +0200

    Cleaunp Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,6 @@ package org.example;
 import org.example.model.*;
 public class Main {
     public static void main(String[] args) {
-        Report r1 = new ReportByProject("data/simple/2012");
-        r1.generate("excel");
-
-        Report r2 = new ReportByPerson("data/simple/2012");
-        r2.generate("excel");
-
-        Report r3 = new ReportByTask("data/simple/2012");
-        r3.generate("excel");
-
         String path;
         String type;
         String output;
