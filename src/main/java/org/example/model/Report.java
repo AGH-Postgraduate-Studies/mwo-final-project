19605781e2cad509d96ea406a40d17b8912bdcf9
msiewierska <marta.siewierska-stanczak@creativestyle.pl>
2024-06-09 14:33:09 +0200

    Print better report

--- src/main/java/org/example/model/Report.java
+++ src/main/java/org/example/model/Report.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public abstract class Report {
@@ -41,18 +42,33 @@ public abstract class Report {
     protected void printErrors() {
         if (errorsList != null && !errorsList.isEmpty()) {
             System.out.println();
-            System.out.println("Errors");
+            System.out.println("-------------------------------------------------------------------------------------");
+            System.out.println();
+
+            System.out.println("Errors found in the following files: ");
             for (Error er : errorsList) {
                 System.out.print("Error in file: " + er.getFileName() + " in sheet: " + er.getSheet());
                 System.out.print(", in row: " + er.getRow() + ", in column: " + er.getColumn());
                 System.out.print(" Error type is: " + er.getType());
                 System.out.println();
             }
+            System.out.println();
+            System.out.println("-------------------------------------------------------------------------------------");
+            System.out.println();
+
         }
     }
 
     public void printToConsole() {
+        Date date = new java.util.Date();
+        System.out.println();
+
+        System.out.println("-------------------------------------------------------------------------------------");
         System.out.println(getDescription());
+        System.out.println("-------------------------------------------------------------------------------------");
+
+        System.out.println("Report generated " + date );
+        System.out.println();
         for (var l : getData(path)) {
             for (var o : l) {
                 System.out.print("    " + o + "\t");
